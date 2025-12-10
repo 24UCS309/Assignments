4478e837738435d12568c2ea3e979b4150405635
vijaya pawar <vijayapawar239@gmail.com>
2025-12-10 22:21:33 +0530

    Arithmetic operation

---
+++ main.py
@@ -0,0 +1,9 @@
+from arithmatic import addition,Substraction
+
+print("Arithmetic OPerations")
+
+num1 = int(input("Enter Number : "))
+num2 = int(input("Enter Number : "))
+
+addition(num1,num2)
+Substraction(num1,num2)
